5efd7d2cf6fe19d3617cbafc29f6d40c83fffc2c
aymantalukder <aymant@nycstudents.net>
2024-11-07 17:33:17 +0000

    first commit

---
+++ GridPath.java
@@ -0,0 +1,19 @@
+public class GridPath {
+
+    private int[][] grid;
+
+    public Location getNextLoc(int row, int col) 
+    {
+    public int sumPath(int row, int col) {
+      }  
+
+    }
+    public GridPath(int[][] values)
+    {
+        grid=values;
+    }
+
+
+
+    
+  }
